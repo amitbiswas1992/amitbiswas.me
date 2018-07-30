0f5e13c5cd9434bb46ff5c74071a2d826a5abb41
Amit Biswas <biswas1992@hotmail.com>
2018-07-30 12:00:20 -0400

    Modified content

--- application/views/page/about.php
+++ application/views/page/about.php
@@ -2,49 +2,55 @@
 <h3 style="text-align: center;">ABOUT ME</h3>
 <br><br>
 <section>
-<h3>Introduction</h3>
-<p>Nicole is a social entrepreneur who is passionate about levelling life's playing-field by making education and medicine accessible to everyone regardless of gender, race, geography, or background.</p>
-<p>Nicole specialises in research-driven product development. She dives deep into a problem, understanding its complexities and subtleties, and designs innovative people-focussed solutions. She has led the product development of a number of projects including Meducation, Exercism, Kaido, and Reachora. Over 200,000 people have benefited from free educational products that Nicole has developed.</p>
-<p>Nicole is the co-founder of Thalamus where she leads a variety of projects, ranging from mentor-based learning for junior software developers to artificial intelligence used during diagnostic heart surgery. Thalamus is a profitable, independently owned company.</p>
-</section>
+<h3>Who I am..</h3>
+
+<!-- <p>Learning for me is always a very exciting venture.I believe that knowledge gained is something no one can take away from me once it is acquired.   In reviewing the course package at BrainStation, I was most excited to see the focus on iOS development.  IOS development is my main passion and I would like to acquire as many new skills through the program.  I spoke in depth about the course components with one of the learning advisors, Mr. Mitch.</p>
+<p>I also gave him a good idea about my programming experience and we spoke about Object Oriented Programming.  He encouraged me to apply to the program and agreed that I would benefit greatly from the courses available. Another aspect of BrainStation’s course design that excited me was the interactive live classes.  It’s always valuable to be able to get direct feedback and ask questions as they arise.  It is also imperative to get direct consultation from professionals in the industry that</p>
+<p>can share real world proficiency. Overall, I really enjoy the way the entire program is constructed and the benefits I will reap after its completion.</p> -->
+<p>I am a 25 year old Entrepreneurial App Developer who has been working in software development since (year).
+Nowadays, I primarily code IOS apps using Xcode and Swift.  While my roots began with more native programming languages, I quickly learning the iOS app development was my passion. </p>
+<p> What keeps me going and interested in this field is the one question that I always ask myself, “How can I make this work?” I have a belief that if I can bring an idea into reality, I can somehow make a contribution to our evolving tech world. </p>
+
 <section>
-<h3>Notable Work</h3>
-<h4>Thalamus</h4>
+<h3>What I believe..</h3>
+<h4></h4>
 <p>
-Nicole co-founded
-<a href='https://thalamus.ai'>Thalamus</a>
-in 2017, with the aim of solving meaningful, real-world problems through the use of artificial intelligence. In its first year, Thalamus has created solutions for
-<a href="http://kaidogroup.com">Kaido</a>,
-<a href="http://redbrickbills.co.uk">RedBrick Bills</a>,
+
+<a href='https://thalamus.ai'></a>
+Over the years, my focus within the computer science space has transformed as my dreams have become bigger and as my knowledge has advanced.  Leaving my mark as an iOS developer drives my intention in all of my work. As a developer, I have become committed to solving problems that I have come to know of or experienced myself and try to create a solution for it.   You see your app idea as a company or a business and you build your app because you believe in that idea. But really my dream is to utilize my skills to make the world a better place by solving real life problems.
+Where I am headed…
+While I don’t know exactly what the future holds, I am interested in building a startup platform that also operates as an app (one component in a multi-interface solution) that can elevate the state of community communication and will have much greater capabilities within them.  My dream is to create something that will streamline the user experience and become and innovative part of our culture.
+
+<a href="http://kaidogroup.com"></a>,
+<a href="http://redbrickbills.co.uk"></a>,
 and
-<a href="http://www.bigwideworld.co/">Big Wide World</a>, and been shortlisted for New Business of the Year.
+<a href="http://www.bigwideworld.co/">Big Wide World</a>,
 </p>
 <p>
-Nicole currently splits her time between building
-<a href="https://reachora.com">Reachora</a>
-- a platform for medical publishers - and developing an artificial intelligence algorithm for use during diagnostic heart-surgery, which if successful will result in improved outcomes for 150,000 patients worldwide each year.
-</p>
-<h4>Meducation</h4>
+
+<a href="https://reachora.com"></a> <h3>Why I love iOS..</h3>
+
+
 <p>
-Nicole joined
-<a href='https://meducation.net'>Meducation</a>
-after their seed round as Head of Product. She led Meducation's development from a small UK site to an internationally respected platform used by 15% of medical students worldwide.
+<a href='https://meducation.net'></a>
+Swift is one of my favorite programming languages that I have utilized in app development.  I especially appreciate its expressiveness and readability and it’s capability to reduce development time and error.  Currently, I am using all of the program’s features and its vast source of dynamic libraries which allow me to create robust, yet adaptable applications.
 </p>
-<p>Meducation provides thousands of free learning resources to medical students, supplementing their formal university learning, and providing access to high-quality teaching material for students from all over the world. It is 100% free.</p>
-<h4>Exercism</h4>
+
+<h3>How I can help…</h3>
 <p>
-<a href='https://v2.exercism.io'>Exercism</a>
-was founded in 2012 by Katrina Owen as a platform to help budding software developers improve their coding skills through mentorship and practice.
+<a href='https://v2.exercism.io'></a>
+<p>If you are in need of a freelance iOS App Developer or would like to talk about an idea you want to push forward, I’m always available to talk.  I believe that part of the development process is about exchanging ideas and brainstorming with other creative types.</p>
+  You can send your inquiries and other internet ephemera to <a href='mailto:info@abiswas.me'>info@abiswas.me</a>.</p>
 </p>
-<p>Thalamus took a 30% stake in the company in 2017. Nicole led a full product redevelopment, designing the platform to be friendly, welcoming and accessible to anyone regardless of geography. Exercism is now used by 150,000 software developers worldwide. It is 100% free.</p>
+<!-- <p>Thalamus took a 30% stake in the company in 2017. Nicole led a full product redevelopment, designing the platform to be friendly, welcoming and accessible to anyone regardless of geography. Exercism is now used by 150,000 software developers worldwide. It is 100% free.</p>
 <h4>Project Management</h4>
 <p>Before entering the startup world, Nicole wanted to get experience of bigger businesses so worked as a project manager on a number of projects, including an £82m infrastructure project for NFU Mutual, a company-wide software upgrade project at Wragge and Co, and a relocation project for Lloyds Banking Group.</p>
-<p>While completing these projects, Nicole qualified as both a PRINCE2 Practitioner and an Accredited Project Management under the APM Foundation.</p>
+<p>While completing these projects, Nicole qualified as both a PRINCE2 Practitioner and an Accredited Project Management under the APM Foundation.</p> -->
 </section>
 <section>
 <h3>Get in touch</h3>
 <p>
-You can reach Nicole on
+You can reach <a href='https://twitter.com/n_chalmers'>Amit</a> on
 <a href='https://twitter.com/n_chalmers'>Twitter</a>
 or
 <a href='mailto:nicole@thalamus.ai'>by email</a>.
